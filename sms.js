af0cd16744f1e3a2a75b74e07a2a5d2e04d99c61
petrachytrasojkova-stack <petra.chytra.sojkova@gmail.com>
2026-07-05 09:13:46 +0200

    Update sms.js

--- sms.js
+++ sms.js
@@ -2,7 +2,16 @@ export function renderSettings(container) {
   container.innerHTML = `
     <section class="card">
       <h2>Nastavení</h2>
-      <p>Nastavení aplikace připravíme později.</p>
+
+      <p>Tato část je ve vývoji.</p>
+
+      <p>Zde budou později:</p>
+
+      <ul>
+        <li>správa hráčů</li>
+        <li>nastavení kategorií cvičení</li>
+        <li>nastavení aplikace</li>
+      </ul>
     </section>
   `;
 }
