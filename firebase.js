57192491e4faf42119f8c4ca1f8756855ff7bd3a
petrachytrasojkova-stack <petra.chytra.sojkova@gmail.com>
2026-07-05 02:57:29 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'DOPLNIT',
-  authDomain: 'DOPLNIT',
-  projectId: 'DOPLNIT',
-  storageBucket: 'DOPLNIT',
-  messagingSenderId: 'DOPLNIT',
-  appId: 'DOPLNIT',
+  apiKey: 'AIzaSyAqqYrnxMeTN8YvAC-60Y6DdAammlIYhqg',
+  authDomain: 'hc-litvinov-2018.firebaseapp.com',
+  projectId: 'hc-litvinov-2018',
+  storageBucket: 'hc-litvinov-2018.firebasestorage.app',
+  messagingSenderId: '1034887165949',
+  appId: '1:1034887165949:web:3d971fe8671656d38b8b94',
 };
 
 const app = initializeApp(firebaseConfig);
